39251f48125f8a12bccef97ebe13b09b4adc329a
madoos <maurice.ronet.dominguez@gmail.com>
2016-12-12 22:41:32 +0100

    refactorized folder sh to bin

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,10 @@
 'use strict';
 
 const yeoman = require('yeoman-generator');
-
 const generatorName = 'madoos-node-module';
 const questions = require('./promptQuestions');
 const templatePath = require('path').join(__dirname, './templates');
-
-
 const yoHelper = require('madoos-yo-helper')(generatorName, questions, templatePath);
-
 
 module.exports = yeoman.Base.extend({
   prompting: yoHelper.prompts,
